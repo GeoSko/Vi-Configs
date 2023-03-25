60343ca7e4d10489c3823113d986c84c0ee475db
GeoSko <bigspazos@gmail.com>
2023-03-25 15:07:32 +0200

    Updated README.MD, added bash script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vi_config
+
+git clone https://github.com/GeoSko/vi_config.git
+cd vi_config
+chmod +x ./setup.sh
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install vim-gui-common
+mkdir -p ~/.vim/colors
+cp monokai.vim ~/.vim/colors
+cp .vimrc ~/.vimrc
