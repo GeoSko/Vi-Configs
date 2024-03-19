9ac4325e6feee04d103d25c5502d0304f2b700e2
Georgios Skondras <gskondras@ac.upatras.gr>
2024-03-19 15:00:03 +0200

    added vim-plug in setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@
 mkdir -p ~/.vim/colors
 cp monokai.vim ~/.vim/colors
 cp .vimrc ~/.vimrc
+
+#for vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
